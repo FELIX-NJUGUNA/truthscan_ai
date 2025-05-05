a51441fb761cbef7fe809fa7c772e8998786a91d
FELIX-NJUGUNA <felixnjuguna443@gmail.com>
2025-05-05 23:07:31 +0300

    Included nltk directory

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Exit immediately on error
+set -e
+
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Download NLTK resources to local folder
+# Create the nltk_data directory explicitly
+mkdir -p nltk_data
+
+# Download NLTK resources
 python -m nltk.downloader -d ./nltk_data punkt averaged_perceptron_tagger
